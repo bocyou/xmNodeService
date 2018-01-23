3e7b17a3a5853b4d03547c398dae9cb9a3555419
ghnice <ghaonice@gmail.com>
2018-01-23 09:32:10 +0800

    code

--- xiaomai_manage/src/app/order-dinner/dinner-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DinnerFilterPipe } from './dinner-filter.pipe';
-
-describe('DinnerFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DinnerFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
