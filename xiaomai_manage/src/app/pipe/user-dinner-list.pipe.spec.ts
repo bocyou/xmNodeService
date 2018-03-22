117786cf6179d70b85c52660db6a5103dba7bdf8
ghnice <ghaonice@gmail.com>
2018-03-22 19:32:15 +0800

    test

--- xiaomai_manage/src/app/pipe/user-dinner-list.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserDinnerListPipe } from './user-dinner-list.pipe';
-
-describe('UserDinnerListPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UserDinnerListPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
