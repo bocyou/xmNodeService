ff238fcf65347f5ff6e82bd2d06e941f95cd4ec6
ghnice <ghaonice@gmail.com>
2018-04-23 21:07:32 +0800

    test xiaomai

--- routes/service.js
+++ routes/service.js
@@ -4,12 +4,21 @@
 var express=require('express');
 var router=express.Router();
 router.get('/',function(req,res){
+    console.log(req.body);
     console.log(req.query.signature);
     console.log(req.query.timestamp);
     console.log(req.query.nonce);
     console.log(req.query.echostr);
 
     res.status(200).send(req.query.echostr)
+
+});
+
+router.post('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log(req.body);
+
+
 
 });
 
