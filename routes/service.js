b9a68cfc80b23427ed45ab5db88e968901f5764b
ghnice <ghaonice@gmail.com>
2018-04-23 20:36:44 +0800

    xiaomai test

--- routes/service.js
+++ routes/service.js
@@ -4,12 +4,12 @@
 var express=require('express');
 var router=express.Router();
 router.get('/',function(req,res){
-    console.log(req.query);
-    console.log(req.param('signature'));
-    console.log(req.param('timestamp'));
-    console.log(req.param('nonce'));
-    console.log(req.param('echostr'));
-    res.status(200).send({code: 200, result:req.param('echostr'), message: "参数校验成功"})
+    console.log(req.query.signature);
+    console.log(req.query.timestamp);
+    console.log(req.query.nonce);
+    console.log(req.query.echostr);
+
+    res.status(200).send(req.query.echostr)
 
 });
 
