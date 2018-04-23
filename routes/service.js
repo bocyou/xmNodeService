bb4b7bafc3984d6aaf89dd6e05234a98e2b153fc
ghnice <ghaonice@gmail.com>
2018-04-23 21:31:58 +0800

    xiaomai test

--- routes/service.js
+++ routes/service.js
@@ -17,9 +17,48 @@ router.get('/',function(req,res){
 router.post('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     console.log(req.body);
+    var access_token='';
+    request.get({
+        url: 'https://api.weixin.qq.com/sns/jscode2session', params: {
+            grant_type: 'client_credential',
+            appid: "wxff898caf09a11846",
+            secret: "6f8b1e6559774ab25c0e6ec3b5b1ee26"
+        }
+    }, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            console.log('获取失败ass');
+            // res.send(200, {code: 200, result: '获取openid失败'});
+        } else {
+
+            access_token=JSON.parse(body).access_token;
+            request.post({
+                url: 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token='+access_token, formData: {
+                    "touser":"OPENID",
+                    "msgtype":"text",
+                    "text":
+                        {
+                            "content":"Hello World"
+                        }
+                }
+
+            }, function optionalCallback(err, httpResponse, body) {
+                if (err) {
+                    console.log('发送失败');
+                   // res.send(200, {code: 200, result: '获取openid失败'});
+                } else {
+                   console.log('发送成功');
 
 
+                }
+
+            });
+
+        }
+
+    });
 
 });
+
+
 
 module.exports=router;
