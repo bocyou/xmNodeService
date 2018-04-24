5e0438697a01969e0ed057761566b9875bf2c2ac
ghnice <ghaonice@gmail.com>
2018-04-24 09:45:36 +0800

    xiaomai test

--- routes/service.js
+++ routes/service.js
@@ -28,6 +28,29 @@ router.post('/', function (req, res, next) {
         } else {
             console.log(JSON.parse(body));
             access_token = JSON.parse(body).access_token;
+            request({
+                url: 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' + access_token,
+                method: "POST",
+                json: true,
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    "touser":user_openid,
+                    "msgtype": "text",
+                    "text":
+                        {
+                            "content": "Hello World"
+                        }
+                })
+            }, function(error, response, body) {
+                console.log(body);
+                if (!error && response.statusCode == 200) {
+                    console.log('发送成功');
+                }
+            });
+
+       /*
             request.post({
                 url: 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' + access_token,
                 form: {
@@ -52,7 +75,7 @@ router.post('/', function (req, res, next) {
 
                 }
 
-            });
+            });*/
 
         }
     });
