1d67419503d528d9b941f89da01687e0dd6b4701
ghnice <ghaonice@gmail.com>
2018-04-25 19:30:31 +0800

    code

--- routes/service.js
+++ routes/service.js
@@ -21,6 +21,8 @@ router.post('/', function (req, res, next) {
     user_openid = req.body.FromUserName || user_openid;
     console.log(req.body);
     var content_txt=req.body.Content;
+    var content_type=req.body.MsgType;
+    var session_form=req.body.SessionFrom;
     // console.log('openid='+user_openid);
     var access_token = '';
     request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxff898caf09a11846&secret=6f8b1e6559774ab25c0e6ec3b5b1ee26', function (err, response, body) {
@@ -31,19 +33,39 @@ router.post('/', function (req, res, next) {
             console.log(JSON.parse(body));
             access_token = JSON.parse(body).access_token;
             var post_parame='';
-            if(content_txt=='pay'){
-                post_parame = JSON.stringify({
-                    "touser": user_openid,
-                    "msgtype": "text",
-                    "text": {"content": '<a href="https://xiaomai.towords.com/paycode">点击获取付款二维码</a>'}
-                });
-            }else{
-                post_parame = JSON.stringify({
-                    "touser": user_openid,
-                    "msgtype": "text",
-                    "text": {"content": '没事别来烦我'}
-                });
+            switch (content_type){
+                case 'event':
+                    if(session_form=='imgcode'){
+                        post_parame = JSON.stringify({
+                            "touser": user_openid,
+                            "msgtype": "text",
+                            "text": {"content": '<a href="https://xiaomai.towords.com/paycode">点击获取付款二维码</a>'}
+                        });
+                    }else{
+                        post_parame = JSON.stringify({
+                            "touser": user_openid,
+                            "msgtype": "text",
+                            "text": {"content": '没事别来烦我'}
+                        });
+                    }
+                    break;
+                default:
+                    if(content_txt=='pay'){
+                        post_parame = JSON.stringify({
+                            "touser": user_openid,
+                            "msgtype": "text",
+                            "text": {"content": '<a href="https://xiaomai.towords.com/paycode">点击获取付款二维码</a>'}
+                        });
+                    }else{
+                        post_parame = JSON.stringify({
+                            "touser": user_openid,
+                            "msgtype": "text",
+                            "text": {"content": '没事别来烦我'}
+                        });
+                    }
+                    break;
             }
+
 
             console.log(post_parame);
             request.post({
@@ -59,47 +81,7 @@ router.post('/', function (req, res, next) {
                     }
                 }
             })
-            /*     request({
-                     url: 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' + access_token,
-                     method: "POST",
-                     json: false,
-                     headers: {
-                         "content-type": "application/json"
-                     },
-                     body:  post_parame
-                 }, function(error, response, body) {
-                     console.log(body);
-                     if (!error && response.statusCode == 200) {
-                         console.log('发送成功');
-                     }
-                 });*/
 
-            /*
-                 request.post({
-                     url: 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' + access_token,
-                     form: {
-                         "touser":user_openid,
-                         "msgtype": "text",
-                         "text":
-                             {
-                                 "content": "Hello World"
-                             }
-                     }
-
-                 }, function optionalCallback(err, response, body) {
-                     console.log('发送请求');
-                     console.log(body);
-
-                     if (err) {
-                         console.log('发送失败');
-                         // res.send(200, {code: 200, result: '获取openid失败'});
-                     } else {
-                         console.log('发送成功');
-
-
-                     }
-
-                 });*/
 
         }
     });
