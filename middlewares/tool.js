b770b5be715c2d25442807926c40c3021cedb263
ghnice <ghaonice@gmail.com>
2018-08-31 11:21:25 +0800

    code

--- middlewares/tool.js
+++ middlewares/tool.js
@@ -5,7 +5,7 @@ var mysql = require('../lib/mysql');
 module.exports = {
     getUserInfo: function(req,res,callback){
         var session=req.headers.sessionkey;
-
+         console.log(session);
         mysql.sql( 'SELECT * FROM custom_session tab1 JOIN users tab2 ON tab1.open_id = tab2.open_id WHERE session_key = "'+session+'"', function (err, result) {
             if(result&&result.length>0){
                 callback(result);
